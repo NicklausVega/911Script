f4dc8368b7e583c53e350b20aecf000aa28bb210
Nick <nick@nicklausvega.tech>
2025-01-22 08:37:38 -0600

    Upload files

---
+++ config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+Config.prefix = '^5[^1911^5] ^3';
+
+Config.cadurl = "https://bright-valley-cad.bvrp.us/"
+
+Config.discord_webhook = "" -- Discord Webhook
---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Mr.Gamer'
+description 'Simple 911 Script'
+version 'v1.1.2'
+
+
+shared_script 'config.lua'
+
+server_script {
+
+     '911script_s.lua',
+     'version_check.lua'
+}
